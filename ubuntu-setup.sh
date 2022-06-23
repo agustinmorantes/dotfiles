357b41cf353aa52b4bb95700a999dc413f6f71d8
Agustín Morantes <agustinmorantes@gmail.com>
2022-06-23 03:00:36 -0300

    Update install script

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -4,6 +4,13 @@
 sudo apt update && sudo apt upgrade -y && \
 xargs -a pkglist sudo apt install -y
 
+# Zsh + OhMyZsh
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
 # Symbolic Links
 CURDIR=$(pwd)
 mv ~/.bashrc ~/.bashrc.old
@@ -16,10 +23,3 @@ ln -sv "$CURDIR/shell/.zshrc" ~
 ln -sv "$CURDIR/git/.gitconfig" ~
 ln -sv "$CURDIR/.profile" ~
 ln -sv "$CURDIR/.profile" ~/.zprofile
-
-# Zsh + OhMyZsh
-chsh -s $(which zsh)
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# Rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh

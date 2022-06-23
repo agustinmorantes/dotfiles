cd5ff0f1785972e1d45d6121978fbc0307d4e57b
Agustín Morantes <agustinmorantes@gmail.com>
2022-06-23 02:32:07 -0300

    Initial Commit

---
+++ ubuntu-setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Install Packages
+sudo apt update && sudo apt upgrade -y && \
+xargs -a pkglist sudo apt install -y
+
+# Symbolic Links
+CURDIR=$(pwd)
+mv ~/.bashrc ~/.bashrc.old
+mv ~/.zshrc ~/.zshrc.old
+mv ~/.gitconfig ~/.gitconfig.old
+mv ~/.profile ~/.profile.old
+mv ~/.zprofile ~/.zprofile.old
+ln -sv "$CURDIR/shell/.bashrc" ~
+ln -sv "$CURDIR/shell/.zshrc" ~
+ln -sv "$CURDIR/git/.gitconfig" ~
+ln -sv "$CURDIR/.profile" ~
+ln -sv "$CURDIR/.profile" ~/.zprofile
+
+# Zsh + OhMyZsh
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh

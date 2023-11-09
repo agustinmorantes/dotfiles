b1a2239f540bb5a568e33d240cf492e72c6f4f3d
Agustín Morantes <agustinmorantes@gmail.com>
2023-11-09 20:37:59 -0300

    Neovim config shorter timeout

--- editor/.config/nvim/lua/custom/config.lua
+++ editor/.config/nvim/lua/custom/config.lua
@@ -3,7 +3,7 @@ vim.wo.relativenumber = true
 -- Set caret offset scrolling
 vim.wo.scrolloff = 10
 
-vim.o.timeoutlen = 500
+vim.o.timeoutlen = 125
 
 vim.diagnostic.config({ update_in_insert = true })
 
